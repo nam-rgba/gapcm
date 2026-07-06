00a32a0279e7c35256c924462acfa937075e573e
nam-rgba <doanngocnam3002@gmail.com>
2026-07-07 05:51:40 +0700

    ui for landing page

--- app/ui/src/pages/home/Home.tsx
+++ app/ui/src/pages/home/Home.tsx
@@ -3,35 +3,32 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import * as React from "react";
 
-
 gsap.registerPlugin(useGSAP);
-const {useRef} = React;
+const { useRef } = React;
 
 export const Home = () => {
-
   const boxRef = useRef<HTMLDivElement>(null);
   const { contextSafe } = useGSAP({});
 
-
-
-  const onClickName = contextSafe(
-    () => {
-          gsap.to(boxRef.current, { x: 350 });
-        }
-  )
+  const onClickName = contextSafe(() => {
+    gsap.to("#g-name", { x: 350 });
+  });
 
   return (
-    <div className="w-full h-full relative">
+    <div className="w-full h-full relative" 
+        ref={boxRef}
+    
+    >
       <div className="absolute top-0 left-0 w-full h-full">
         <Experience />
       </div>
       <div
         onClick={onClickName}
-        ref={boxRef}
+        id="g-name"
         className="absolute top-0 left-0  text-white text-4xl font-bold  cursor-pointer"
-      >
-        Nam
+      > 
+          Nam đang code
       </div>
     </div>
   );
-};
+} 
